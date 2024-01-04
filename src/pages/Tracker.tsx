800599fed9151f53f51ee8ccd61de1d84eddf5f3
RemiSaurel <r.saurel5@gmail.com>
2024-01-04 10:46:05 +0100

    feat(pages): add router

---
+++ src/pages/Tracker.tsx
@@ -0,0 +1,7 @@
+export function Tracker() {
+  return (
+    <div>
+      <div className="text-2xl font-semibold">Tracker</div>
+    </div>
+  );
+}
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,6 +3,8 @@ import Dashboard from "@/pages/Dashboard.tsx";
 import Home from "@/pages/Home.tsx";
 import { AuthLayout } from "@/pages/AuthLayout.tsx";
 import { Layout } from "@/pages/Layout.tsx";
+import { Tracker } from "@/pages/Tracker.tsx";
+import { Analysis } from "@/pages/Analysis.tsx";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +22,14 @@ export const router = createBrowserRouter([
           {
             path: "/dashboard",
             element: <Dashboard />,
+          },
+          {
+            path: "/tracker",
+            element: <Tracker />,
+          },
+          {
+            path: "/analysis",
+            element: <Analysis />,
           },
         ],
       },
