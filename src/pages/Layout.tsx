1e6bf8ebca7c7fc4f84ac2c1a06bbc141644cad9
RemiSaurel <r.saurel5@gmail.com>
2024-01-12 20:59:15 +0100

    feat(teams): add TanstackQuery to app

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
 import { VerticalNavbar } from "@/components/navbar/VerticalNavbar.tsx";
 import { useAuth } from "@/hooks/useAuth.ts";
+import { Toaster } from "@/components/ui/sonner.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+export const queryClient = new QueryClient();
 export function Layout() {
   // Get user
   const { user } = useAuth();
   return (
-    <div className="w-full h-full flex">
-      <VerticalNavbar user={user} />
-      <div className="p-6 w-full overflow-auto">
-        <Outlet />
+    <QueryClientProvider client={queryClient}>
+      <div className="w-full h-full flex">
+        <Toaster closeButton position="top-right" />
+        <VerticalNavbar user={user} />
+        <div className="p-6 w-full overflow-auto">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
