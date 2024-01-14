5e9fd7a38b6306a10d5bb0b053b5552573e36d47
RemiSaurel <r.saurel5@gmail.com>
2024-01-14 09:31:51 +0100

    feat(tracker): tracker service add result

--- src/services/tracker-service.ts
+++ src/services/tracker-service.ts
@@ -1,17 +1,33 @@
 import ky from "ky";
 import { BASE_URL } from "@/utils/constants.ts";
 import { TrackerResult } from "@/models/tracker/columns.tsx";
+import { FormDataTrackerResult } from "@/models/overwatch/maps.ts";
 
-export async function getTrackerResults(
-  teamName: string,
-): Promise<TrackerResult[]> {
-  if (!teamName) {
-    throw new Error("Team name is required");
+async function getTrackerResults(teamId: number): Promise<TrackerResult[]> {
+  if (!teamId) {
+    throw new Error("Team id is required");
   }
 
   return await ky
-    .get(`${BASE_URL}/tracker/${teamName}`, {
+    .get(`${BASE_URL}/tracker/${teamId}`, {
       credentials: "include",
     })
     .json();
 }
+
+async function addTrackerResult(
+  teamId: number,
+  trackerResult: FormDataTrackerResult,
+): Promise<void> {
+  return await ky
+    .post(`${BASE_URL}/tracker/${teamId}`, {
+      credentials: "include",
+      json: trackerResult,
+    })
+    .json();
+}
+
+export const trackerService = {
+  getTrackerResults,
+  addTrackerResult,
+};
