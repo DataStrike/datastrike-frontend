220a66ec96b93124955d66f56aefd2e204da5b84
RemiSaurel <r.saurel5@gmail.com>
2024-01-18 02:22:38 +0100

    feat(analysis): service

---
+++ src/services/maps-service.ts
@@ -0,0 +1,26 @@
+import ky from "ky";
+import { BASE_URL } from "@/utils/constants.ts";
+import { AnalysisMap } from "@/models/analysis/analysismaps.ts";
+
+async function getMaps(teamId: number): Promise<AnalysisMap[]> {
+  if (!teamId) {
+    throw new Error("Team id is required");
+  }
+
+  return await ky
+    .get(`${BASE_URL}/maps/${teamId}`, {
+      credentials: "include",
+    })
+    .json();
+}
+async function addAnalysisMaps(teamId: number, maps: FormData): Promise<void> {
+  await ky.post(`${BASE_URL}/maps/${teamId}`, {
+    credentials: "include",
+    body: maps,
+  });
+}
+
+export const mapsService = {
+  addAnalysisMaps,
+  getMaps,
+};
