c6c7d9672660f806919cec7aa475b2b63755ee74
RemiSaurel <r.saurel5@gmail.com>
2024-01-25 09:43:36 +0100

    feat(scouting): useQuery

--- src/pages/ScoutingPlayer.tsx
+++ src/pages/ScoutingPlayer.tsx
@@ -1,40 +1,23 @@
-import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { scoutingService } from "@/services/scouting-service.ts";
-import { HeroStats, PlayerData } from "@/models/stats/models.ts";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table.tsx";
 import { ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button.tsx";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs.tsx";
+import { useQuery } from "@tanstack/react-query";
 
 export function ScoutingPlayer() {
-  const [playerData, setPlayerData] = useState<PlayerData | null>(null);
   const { playerId } = useParams();
   const navigate = useNavigate();
 
-  const getPlayerSummary = async (playerId: string) => {
-    try {
-      const data: PlayerData = await scoutingService.getPlayerStats(playerId);
-      setPlayerData(data);
-    } catch (error) {
-      console.error("Error fetching player scouting:", error);
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      if (playerId) {
-        await getPlayerSummary(playerId);
-      }
-    };
-
-    fetchData();
-  }, [playerId]);
+  const { data: playersStats, isFetching: playersStatsLoading } = useQuery({
+    queryKey: ["players", playerId],
+    queryFn: () => scoutingService.getPlayerStats(playerId!),
+  });
 
   return (
     <div className="flex flex-col gap-4">
@@ -52,89 +35,47 @@ export function ScoutingPlayer() {
         <div className="text-2xl font-semibold">Player stats</div>
       </div>
 
-      <div className="flex gap-2 w-full">
-        {/* Display hero-specific scouting */}
-        {playerData?.heroes && (
-          <HeroStatsContainer heroData={playerData.heroes || {}} />
-        )}
-      </div>
+      {playersStatsLoading && <div>Loading...</div>}
+      {!playersStatsLoading && (
+        <Tabs defaultValue="general">
+          <TabsList className="w-[400px]">
+            <TabsTrigger className="w-full" value="general">
+              General
+            </TabsTrigger>
+            <TabsTrigger className="w-full" value="heroes">
+              Heroes
+            </TabsTrigger>
+            <TabsTrigger className="w-full" value="roles">
+              Roles
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent
+            className="w-full h-full overflow-auto flex-col"
+            value="general"
+          >
+            {playersStats?.general && (
+              <pre>{JSON.stringify(playersStats.general, null, 2)}</pre>
+            )}
+          </TabsContent>
+
+          <TabsContent
+            className="w-full h-full overflow-auto flex-col"
+            value="heroes"
+          >
+            {playersStats?.heroes && (
+              <pre>{JSON.stringify(playersStats.heroes, null, 2)}</pre>
+            )}
+          </TabsContent>
+          <TabsContent
+            className="w-full h-full overflow-auto flex-col"
+            value="roles"
+          >
+            {playersStats?.roles && (
+              <pre>{JSON.stringify(playersStats.roles, null, 2)}</pre>
+            )}
+          </TabsContent>
+        </Tabs>
+      )}
     </div>
   );
 }
-
-interface HeroStatsContainerProps {
-  heroData: Record<string, HeroStats>;
-}
-
-const HeroStatsContainer: React.FC<HeroStatsContainerProps> = ({
-  heroData,
-}) => {
-  // Extract column keys dynamically from the HeroStats type
-  const columnKeys = Object.keys(heroData[Object.keys(heroData)[0]]).map(
-    (key) => key as keyof HeroStats,
-  );
-
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableCell>Hero</TableCell>
-          {columnKeys.map((key) => (
-            <TableCell key={key}>{key}</TableCell>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {Object.entries(heroData).map(([heroName, heroStats]) => (
-          <HeroStatsRow
-            key={heroName}
-            heroName={heroName}
-            heroStats={heroStats}
-            columnKeys={columnKeys}
-          />
-        ))}
-      </TableBody>
-    </Table>
-  );
-};
-
-interface HeroStatsRowProps {
-  heroName: string;
-  heroStats: HeroStats;
-  columnKeys: (keyof HeroStats)[];
-}
-
-const HeroStatsRow: React.FC<HeroStatsRowProps> = ({
-  heroName,
-  heroStats,
-  columnKeys,
-}) => (
-  <TableRow>
-    <TableCell>{heroName}</TableCell>
-    {columnKeys.map((key) => (
-      <TableCell key={key}>
-        {key.includes(".")
-          ? getObjectValue(heroStats, key)
-          : renderValue(heroStats[key])}
-      </TableCell>
-    ))}
-  </TableRow>
-);
-
-// Helper function to get nested object value
-const getObjectValue = (obj: any, path: string) => {
-  const pathArray = path.split(".");
-  let value = obj;
-  for (const key of pathArray) {
-    value = value[key];
-  }
-  return value;
-};
-
-// Helper function to render a value (convert object to string)
-const renderValue = (value: any) => {
-  if (typeof value === "object") {
-    return JSON.stringify(value);
-  }
-  return String(value);
-};
