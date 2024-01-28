d9d997883a44d79b8e1888c8d4a4c8d73f619921
RemiSaurel <r.saurel5@gmail.com>
2024-01-28 11:54:27 +0100

    chore(stats): refactor stats + admin (wip)

---
+++ src/components/admin/TableContainer.tsx
@@ -0,0 +1,13 @@
+import { Fragment, ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+  title: string;
+}
+export function TableContainer({ children, title }: Props) {
+  return (
+    <div className="flex flex-col gap-1">
+      <span className="text-xl font-bold">{title}</span>
+      <Fragment>{children}</Fragment>
+    </div>
+  );
+}
