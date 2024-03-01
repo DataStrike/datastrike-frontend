05cd8ef1cd0f479571d230b3769a3a3d32dd87e8
RemiSaurel <r.saurel5@gmail.com>
2024-03-01 13:50:01 +0100

    feat(fights): add cool fights feature

--- src/components/analysis/fights/FightsContainer.tsx
+++ src/components/analysis/fights/FightsContainer.tsx
@@ -11,6 +11,7 @@ import {
   detectFirstKills,
   getFightMetaData,
   parseDescription,
+  getKeyValuesInfos,
 } from "@/utils/analysis/timeline.ts";
 import { Separator } from "@/components/ui/separator.tsx";
 import {
@@ -21,88 +22,45 @@ import {
 } from "@/components/ui/card.tsx";
 import kill from "@/assets/analysis/kill.svg";
 import ultimate from "@/assets/analysis/ultimate.svg";
+import { countOccurrences, flattenArray, secToMin } from "@/utils/functions.ts";
+import { ArrowRight } from "lucide-react";
 interface Props {
   data: Data;
 }
 
 export function FightsContainer({ data }: Props) {
-  const fights = detectFights(data, 10);
-  const firstKillers = detectFirstKills(fights);
-  const firstDeaths = detectFirstDeaths(fights);
+  const fightsData = detectFights(data, 10);
+  const firstKillersNames = fightsData.map((fights) =>
+    detectFirstKills(fights.fights),
+  );
+  const firstDeathsNames = fightsData.map((fights) =>
+    detectFirstDeaths(fights.fights),
+  );
 
-  const fightsMetaData = fights.map((fight) => getFightMetaData(fight));
+  const fightsMetaData = fightsData.map((fights) =>
+    fights.fights.map((fight) => getFightMetaData(fight)),
+  );
 
-  /*const playerNames = Object.values(mapData.data.rounds[0].teams).map((team) =>
-    Object.values(team.players).map((player) => player.name),
-  );*/
+  const firstKillers = getKeyValuesInfos(
+    countOccurrences(flattenArray(firstKillersNames)),
+  );
+
+  const firstDeaths = getKeyValuesInfos(
+    countOccurrences(flattenArray(firstDeathsNames)),
+  );
 
   return (
     <div className="w-full">
-      <h2 className="text-xl font-bold">Fights</h2>
       <div className="flex flex-col gap-4 ">
-        <div className="flex gap-2 w-full flex-wrap">
-          {fights.map((fight, index) => (
-            <Card className={"h-fit"} key={index}>
-              <CardHeader className="w-full px-4">
-                <CardTitle className="w-full flex justify-between">
-                  <span>Fight {index}</span>
-                  <div className="flex gap-4">
-                    <span className="flex">
-                      <img src={kill} className="w-6 h-6" alt={"kill"} />
-                      {fightsMetaData[index].nbKills}
-                    </span>
-                    <span className="flex">
-                      <img
-                        src={ultimate}
-                        className="w-6 h-6 mr-1.5"
-                        alt={"ultimate"}
-                      />
-                      {fightsMetaData[index].nbUltimates}
-                    </span>
-                  </div>
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <Accordion className={"w-80"} type="single" collapsible>
-                  <AccordionItem
-                    className={"border-b-0"}
-                    value={"item-" + index}
-                    key={index}
-                  >
-                    <AccordionTrigger>Details</AccordionTrigger>
-                    <AccordionContent>
-                      {fight.map((event, index) => (
-                        <div key={index}>
-                          <div className="flex gap-2">
-                            <span>
-                              {parseDescription(event.description).player1}
-                            </span>
-                            <span>
-                              {parseDescription(event.description).action}
-                            </span>
-                            <span>
-                              {parseDescription(event.description).player2}
-                            </span>
-                            <span>
-                              {parseDescription(event.description).keyword}
-                            </span>
-                          </div>
-                        </div>
-                      ))}
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-
         <div className="flex gap-4">
           <div>
             <h2 className="text-xl font-bold">First Killers</h2>
             <div>
               {firstKillers.map((player, index) => (
-                <p key={index}>{player}</p>
+                <div className={"flex justify-between"} key={"player-" + index}>
+                  <p>{player.key}</p>
+                  <p>{player.value}</p>
+                </div>
               ))}
             </div>
           </div>
@@ -111,10 +69,92 @@ export function FightsContainer({ data }: Props) {
             <h2 className="text-xl font-bold">First Deaths</h2>
             <div>
               {firstDeaths.map((player, index) => (
-                <p key={index}>{player}</p>
+                <div className={"flex justify-between"} key={"player-" + index}>
+                  <p>{player.key}</p>
+                  <p>{player.value}</p>
+                </div>
               ))}
             </div>
           </div>
+        </div>
+        <Separator />
+        <div className="flex gap-4 w-full flex-wrap">
+          {fightsData.map((fights, index) => (
+            <div className={"flex flex-col gap-4"} key={index}>
+              <h2 className="text-xl font-bold">
+                Round {fights.roundNumber + 1}
+              </h2>
+              <div className="flex gap-4 flex-wrap" key={index}>
+                {fights.fights.map((fight, j) => (
+                  <Card className={"h-fit"} key={j}>
+                    <CardHeader className="w-full px-4">
+                      <CardTitle className="w-full flex justify-between">
+                        <span className="flex items-center">
+                          {secToMin(fight[0].timestamp)}s
+                          <ArrowRight className="w-4 h-4 mx-2" />
+                          {secToMin(fight[fight.length - 1].timestamp)}s
+                        </span>
+                        <div className="flex gap-4">
+                          <span className="flex">
+                            <img src={kill} className="w-6 h-6" alt={"kill"} />
+                            {fightsMetaData[index][j].nbKills}
+                          </span>
+                          <span className="flex">
+                            <img
+                              src={ultimate}
+                              className="w-6 h-6 mr-1.5"
+                              alt={"ultimate"}
+                            />
+                            {fightsMetaData[index][j].nbUltimates}
+                          </span>
+                        </div>
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent className={"pb-0"}>
+                      <Accordion className={"w-80"} type="single" collapsible>
+                        <AccordionItem
+                          className={"border-b-0"}
+                          value={"item-" + index}
+                          key={index}
+                        >
+                          <AccordionTrigger>Details</AccordionTrigger>
+                          <AccordionContent>
+                            {fight.map((event, index) => (
+                              <div key={index}>
+                                <div className="flex gap-2">
+                                  <span>
+                                    {
+                                      parseDescription(event.description)
+                                        .player1
+                                    }
+                                  </span>
+                                  <span>
+                                    {parseDescription(event.description).action}
+                                  </span>
+                                  <span>
+                                    {
+                                      parseDescription(event.description)
+                                        .player2
+                                    }
+                                  </span>
+                                  <span>
+                                    {
+                                      parseDescription(event.description)
+                                        .keyword
+                                    }
+                                  </span>
+                                </div>
+                              </div>
+                            ))}
+                          </AccordionContent>
+                        </AccordionItem>
+                      </Accordion>
+                    </CardContent>
+                  </Card>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
