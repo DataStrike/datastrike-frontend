3f7e924890c06b732504928c14b96e510c6b1620
RemiSaurel <r.saurel5@gmail.com>
2024-03-01 13:50:15 +0100

    chore(fights): utils functions

--- src/utils/analysis/timeline.ts
+++ src/utils/analysis/timeline.ts
@@ -1,26 +1,68 @@
 import { Data, DataEvent } from "@/models/analysis/analysismaps.ts";
-export function detectFights(data: Data, time: number) {
-  const fights = [];
+
+export interface Fights {
+  roundNumber: number;
+  fights: DataEvent[][];
+}
+
+function getLastTimestampEvent(events: DataEvent[]) {
+  return events[events.length - 1].timestamp;
+}
+
+export function detectFights(data: Data, time: number): Fights[] {
+  let roundNumber = 0;
+
+  const toReturn: Fights[] = [
+    {
+      roundNumber: roundNumber, // get index of round
+      fights: [],
+    },
+  ];
   let fight = [];
   let lastTime = 0;
   const interestingEvents = ["kill", "ultimate"];
 
   for (const event of data.events) {
+    // Check if round has changed
+    const round = data.rounds?.[roundNumber]; // Using optional chaining to handle undefined data.rounds
+
+    if (!round) {
+      break;
+    }
+    const eventTime = Number(event.timestamp);
+    const startTime = Number(round.start_time ? round.start_time : 0);
+    const endTime = Number(
+      round.end_time ? round.end_time : getLastTimestampEvent(data.events),
+    );
+
+    if (eventTime > endTime) {
+      // Push the last fight of the round
+      toReturn[roundNumber].fights.push(fight);
+      fight = [];
+      roundNumber++;
+      toReturn.push({
+        roundNumber: roundNumber,
+        fights: [],
+      });
+    }
+
     // If the event is not a kill or an ultimate, we skip the event
     if (!interestingEvents.includes(event.type)) {
       continue;
     }
-    const eventTime = Number(event.timestamp);
 
-    // If the time between the last event and this event is greater than {time} seconds,
-    // we consider it a new fight
+    // If the time between the last event and this event is greater than {time} seconds, we consider it's a new fight
     if (
       lastTime !== 0 &&
       eventTime - lastTime > time &&
+      eventTime > startTime &&
       interestingEvents.includes(event.type)
     ) {
-      fights.push(fight);
-      fight = [];
+      // If the fight is not empty, we push it to the fights array
+      if (fight.length > 0) {
+        toReturn[roundNumber].fights.push(fight);
+        fight = [];
+      }
     }
 
     if (interestingEvents.includes(event.type)) {
@@ -30,7 +72,7 @@ export function detectFights(data: Data, time: number) {
     lastTime = eventTime;
   }
 
-  return fights;
+  return toReturn;
 }
 
 // Parse the description of an event to extract the character name
@@ -59,3 +101,18 @@ export function detectFirstDeaths(fights: DataEvent[][]) {
     }
   });
 }
+
+export function getFightMetaData(fight: DataEvent[]) {
+  const nbKills = fight.filter((event) => event.type === "kill").length;
+  const nbUltimates = fight.filter((event) => event.type === "ultimate").length;
+  return { nbKills, nbUltimates };
+}
+
+export const getKeyValuesInfos = (countsObject: { [key: string]: number }) => {
+  return Object.entries(countsObject)
+    .map(([key, value]) => ({
+      key,
+      value,
+    }))
+    .sort((a, b) => b.value - a.value);
+};
