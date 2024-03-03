4a5676be6cc413d5286476a70562be9fdff95554
RemiSaurel <r.saurel5@gmail.com>
2024-03-03 12:33:23 +0100

    feat: events

--- src/components/analysis/comps/CompsContainer.tsx
+++ src/components/analysis/comps/CompsContainer.tsx
@@ -1,72 +1,51 @@
-import {
-  Data,
-  Round,
-  Player,
-  Character,
-} from "@/models/analysis/analysismaps.ts";
+import { Data, Round } from "@/models/analysis/analysismaps.ts";
 
 interface Props {
   data: Data;
 }
 
 export function CompsContainer({ data }: Props) {
-  const compositions: {
-    [round: number]: {
-      [team: string]: {
-        [player: string]: {
-          character: string;
-          start_time: number;
-          end_time: number;
-        }[];
+  /*[
+    { "round": 0, "start": "0.00", "end": "190.71" },
+    { "round": 1, "start": "190.71", "end": "380.32" },
+    { "round": 2, "start": "380.32", "end": "490.12" }
+  ];*/
+
+  const getRoundsInfo = (rounds: Round[]) => {
+    return rounds.map((round: Round) => {
+      return {
+        round: Array.prototype.indexOf.call(rounds, round),
+        start: round.start_time,
+        end: round.end_time,
       };
-    };
-  } = {};
-
-  // each round
-  data.rounds.forEach((round: Round, roundIndex: number) => {
-    compositions[roundIndex] = {};
-
-    // each team
-    Object.values(round.teams).forEach((team) => {
-      compositions[roundIndex][team.name] = {};
-
-      // each player in the team
-      Object.values(team.players).forEach((player: Player) => {
-        compositions[roundIndex][team.name][player.name] = [];
-
-        // each character played by the player
-        Object.values(player.characters).forEach((character: Character) => {
-          let currentRecord: {
-            character: string;
-            start_time: number;
-            end_time: number;
-          };
-
-          // each played time for the character
-          character.played_time.forEach((time) => {
-            const start = Number(time.start);
-            const end = Number(time.end);
-            if (end - start > 0.5) {
-              currentRecord = {
-                character: character.name,
-                start_time: start,
-                end_time: end,
-              };
-              compositions[roundIndex][team.name][player.name].push(
-                currentRecord,
-              );
-            }
-          });
-        });
-      });
     });
-  });
+  };
+
+  console.log(getRoundsInfo(data.rounds));
+
+  const hero_spawn = data.events.filter((event) => event.type === "hero_spawn");
+  const hero_swaps = data.events.filter((event) => event.type === "hero_swap");
+
+  console.log(hero_spawn, hero_swaps);
 
   return (
     <div className="w-full">
       <div className="flex flex-col gap-4">Comps</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Round</th>
+            <th>Start time</th>
+            <th>End time</th>
+            <th>Team 1 composition</th>
+            <th>Team 2 composition</th>
+          </tr>
+        </thead>
+      </table>
       <pre>
-        <code>{JSON.stringify(compositions, null, 2)}</code>
+        {/*
+        <code>{JSON.stringify(allCompositionRecords, null, 2)}</code>
+*/}
       </pre>
     </div>
   );
