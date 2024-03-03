d0b416d8d2b96f7e64905744cfbf16ae3cfbb1f9
RemiSaurel <r.saurel5@gmail.com>
2024-03-03 13:04:12 +0100

    feat(comps): get comps

--- src/components/analysis/comps/CompsContainer.tsx
+++ src/components/analysis/comps/CompsContainer.tsx
@@ -1,4 +1,5 @@
-import { Data, Round } from "@/models/analysis/analysismaps.ts";
+import { Data, DataEvent, Round } from "@/models/analysis/analysismaps.ts";
+import { getPlayerNames } from "@/utils/analysis/timeline.ts";
 
 interface Props {
   data: Data;
@@ -21,13 +22,44 @@ export function CompsContainer({ data }: Props) {
     });
   };
 
-  console.log(getRoundsInfo(data.rounds));
+  const players = getPlayerNames(data);
 
   const hero_spawn = data.events.filter((event) => event.type === "hero_spawn");
   const hero_swaps = data.events.filter((event) => event.type === "hero_swap");
 
-  console.log(hero_spawn, hero_swaps);
+  // Get the heros played by each player
+  // Players is {team1: { player1: [heroes], player2: [heroes] }, team2: { player1: [heroes], player2: [heroes] }}
+  // be careful, heroes can be swap and I need all the heroes played by each player
 
+  const getHeroesForPlayer = (
+    player: string,
+    spawnEvents: DataEvent[],
+    swapEvents: DataEvent[],
+  ) => {
+    const playerEvents = [
+      ...spawnEvents.filter((event) => event.player === player),
+      ...swapEvents.filter((event) => event.player === player),
+    ];
+    return Array.from(new Set(playerEvents.map((event) => event.hero)));
+  };
+
+  const heroesPlayed = (players: { team1: string[]; team2: string[] }) => {
+    const team1Heroes = players.team1.map((player) => ({
+      player,
+      heroes: getHeroesForPlayer(player, hero_spawn, hero_swaps),
+    }));
+
+    const team2Heroes = players.team2.map((player) => ({
+      player,
+      heroes: getHeroesForPlayer(player, hero_spawn, hero_swaps),
+    }));
+
+    return { team1: team1Heroes, team2: team2Heroes };
+  };
+
+  const allCompositionRecords = heroesPlayed(players);
+  console.log(allCompositionRecords);
+  console.log(getRoundsInfo(data.rounds));
   return (
     <div className="w-full">
       <div className="flex flex-col gap-4">Comps</div>
